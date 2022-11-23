2f0705ab5d61b2246100db4861eed1ef00d8a332
Elizabeth <mwendemuthusi6@gmail.com>
2022-11-23 19:11:14 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// divide function expression
+const divide = function () {
+    return 2000/100;
+}
+console.log(divide());
+
+// square arrow function
+const square = x => x * x;
+console.log(square(20));
+
+// add arrow function
+const add = (a, b) => a + b;
+console.log(add(2, 4));
